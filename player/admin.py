d87a92c6c7a9c7c6a979980338f8826ac6c5d91b
Allison-Northrop <agnorthrop@gmail.com>
2017-07-09 11:30:53 -0700

    succeffully migrated player attributes

--- player/admin.py
+++ player/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Player_Attributes
+
+admin.site.register(Player_Attributes)
 
 # Register your models here.
--- player/models.py
+++ player/models.py
@@ -4,10 +4,11 @@ from django.utils import timezone
 # Create your models here.
 class Player_Attributes(models.Model):
     username = models.CharField(max_length=100)
-    skill_level = models.DecimalField(..., max_digits=2, decimal_places=2)
+    skill_level = models.DecimalField(max_digits=2, decimal_places=2)
     address = models.CharField(max_length=500)
     availability_days = models.CharField(max_length=500)
     availability_times = models.CharField(max_length=500)
     looking_for = models.CharField(max_length=500)
-    contact_info = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(max_length=254)
     additional_info = models.TextField()
