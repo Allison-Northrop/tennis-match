a7675e6aa8c480e79ef756c2e5ea4b90b66553a8
Allison-Northrop <agnorthrop@gmail.com>
2017-07-10 15:29:06 -0700

    form showing up, but not saving to database'

--- player/urls.py
+++ player/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.player_signin, name='player_signin'),
-    url(r'^player_attributes/new$', views.player_attributes_new, name='player_attributes_new'),
+    url(r'^player_attributes/new/$', views.player_attributes_new, name='player_attributes_new'),
 ]
--- player/views.py
+++ player/views.py
@@ -6,13 +6,13 @@ def player_signin(request):
     return render(request, 'player/player_signin.html', {})
 
 def player_attributes_new(request):
-    if request.method == "POST":
-        form = Player_AttributesForm(request.POST)
-        if form.is_valid():
-            player = form.save(commit=False)
-            # TODO come back here and fill out stuff that will automatically populate like the name
-            player.save()
-            return redirect('player_detail', pk=player.pk)
-        else:
-            form = Player_AttributesForm()
-        return render(request, 'player/player_attributes_edit.html', {'form': form})
+    # if request.method == "POST":
+    #     form = Player_AttributesForm(request.POST)
+    # if form.is_valid():
+    #   player = form.save(commit=False)
+    #         # TODO come back here and fill out stuff that will automatically populate like the name
+    #     player.save()
+    #     return redirect('player_detail', pk=player.pk)
+    # else:
+    form = Player_AttributesForm()
+    return render(request, 'player/player_attributes_edit.html', {'form': form})
