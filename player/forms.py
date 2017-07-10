57e6131141220a1cab5c3b8eb02412249686afc0
Allison-Northrop <agnorthrop@gmail.com>
2017-07-10 13:55:35 -0700

    created new player post method. Html still not returning form, but getting there.

---
+++ player/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Player_Attributes
+
+class Player_AttributesForm(forms.ModelForm):
+    class Meta:
+        model = Player_Attributes
+        fields = ('username', 'skill_level', 'address', 'availability_days', 'availability_times', 'looking_for', 'phone_number', 'email', 'additional_info',)
--- player/views.py
+++ player/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import Player_AttributesForm
 
 # Create your views here.
 def player_signin(request):
     return render(request, 'player/player_signin.html', {})
+
+def player_attributes_new(request):
+    if request.method == "POST":
+        form = Player_AttributesForm(request.POST)
+        if form.is_valid():
+            player = form.save(commit=False)
+            # TODO come back here and fill out stuff that will automatically populate like the name
+            player.save()
+            return redirect('player_detail', pk=player.pk)
+        else:
+            form = Player_AttributesForm()
+        return render(request, 'player/player_attributes_edit.html', {'form': form})
