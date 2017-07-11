d5ead52a0b80abe1b0cd1eb3f7a67f2604872501
Allison-Northrop <agnorthrop@gmail.com>
2017-07-11 14:19:59 -0700

    finally got an indivudual player details popping up. fixed url bug and added player details method in view

--- player/views.py
+++ player/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .forms import Player_AttributesForm
 from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def player_signin(request):
     return render(request, 'player/player_signin.html', {})
+
+def player_details(request, pk):
+    # player_object = get_object_or_404(Player, pk=pk)
+    return render(request, 'player/player_details.html', {})
 
 def player_attributes_new(request):
     if request.method == "POST":
@@ -12,7 +17,7 @@ def player_attributes_new(request):
         if form.is_valid():
             player = form.save()
             player.save()
-            # return redirect('player_detail', pk=player.pk)
+            return redirect(player_details, pk=player.pk)
     else:
         form = Player_AttributesForm()
     return render(request, 'player/player_attributes_edit.html', {'form': form})
