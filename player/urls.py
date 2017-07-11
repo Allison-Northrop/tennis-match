dee8ba3196fcffbd832c76d7cc98ae99242b0765
Allison-Northrop <agnorthrop@gmail.com>
2017-07-10 23:09:10 -0700

    creating new branch, so commiting

--- player/urls.py
+++ player/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.player_signin, name='player_signin'),
     url(r'^player_attributes/new/$', views.player_attributes_new, name='player_attributes_new'),
+    #do I put a url for the sign in in here?
 ]
