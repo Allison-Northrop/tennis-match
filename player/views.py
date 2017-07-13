b7f042f64c2b83f5a5986334366101d4fda7e826
Allison-Northrop <agnorthrop@gmail.com>
2017-07-13 15:46:01 -0700

    added more to player summary template

--- player/views.py
+++ player/views.py
@@ -12,7 +12,7 @@ def player_signin(request):
 def player_details(request, pk):
     #do i need to find here?
     player_object = get_object_or_404(Player_Attributes, pk=pk)
-    players = Player_Attributes.objects.filter(pk=pk)
+    players = Player_Attributes.objects.filter(pk=pk) #note to self, this pk=pk thing is the way to get individual items specific to the user popping up.
     return render(request, 'player/player_details.html', {'players': players})
 
 def player_attributes_new(request):
