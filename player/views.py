f74083b1542facf79992077eb31ad0d3855f9fec
Allison-Northrop <agnorthrop@gmail.com>
2017-07-21 15:27:17 -0700

    changed permissions with login

--- player/views.py
+++ player/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+# @login_required
 def player_signin(request):
     return render(request, 'player/player_signin.html', {})
 
@@ -18,11 +18,13 @@ def player_map(request):
     players = Player_Attributes.objects.filter()
     return render(request, 'player/player_map.html', {'players': players})
 
+@login_required
 def player_details(request, pk):
     player_object = get_object_or_404(Player_Attributes, pk=pk)
     players = Player_Attributes.objects.filter(pk=pk) #note to self, this pk=pk thing is the way to get individual items specific to the user popping up.
     return render(request, 'player/player_details.html', {'players': players})
 
+@login_required
 def player_attributes_new(request):
     if request.method == "POST":
         form = Player_AttributesForm(request.POST)
