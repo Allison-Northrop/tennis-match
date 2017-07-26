84a13e7cc6351789cc7129bf3c4080e2ca29dfe2
Allison-Northrop <agnorthrop@gmail.com>
2017-07-26 09:16:33 -0700

    trying to push to python anywhere

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,5 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ["SECRET_KEY"] = 'tv8rn9m5y7@#q%n@vu6kxvrz+go&+79y+_#*%9qx8#2qw%p+ww'
+
+os.environ["SOCIAL_AUTH_TWITTER_KEY"] = 'TGyJSOCXh4Z0pDVpf6fNzgeff'
 
 application = get_wsgi_application()
